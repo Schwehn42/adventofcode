cb67c3f7b51c1260933f3f05d11c8336ebbb7c62
Jakob Schwehn <schwehn42@gmail.com>
2015-12-07 19:19:53 +0100

    day5

--- adventofcode/src/puzzles/Day3.java
+++ adventofcode/src/puzzles/Day3.java
@@ -4,7 +4,9 @@ import util.FileRead;
 
 public class Day3 {
 	public static void run() {
-		String content = FileRead.read("C:\\Users\\schwe\\Desktop\\file.xyz");
-		System.out.println(content);
+		String content = FileRead.read("src/input_data/day3_input.txt");
+		char[] parts = content.toCharArray();
+		System.out.println(parts.length);
+		
 	}
 }
