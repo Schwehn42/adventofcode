3ca3b38ad64bbfb67fba9bba4ca01aea637c1ac5
Jakob Schwehn <schwehn42@gmail.com>
2015-12-12 17:26:33 +0100

    validation

--- adventofcode/src/puzzles/Day11.java
+++ adventofcode/src/puzzles/Day11.java
@@ -3,25 +3,19 @@ package puzzles;
 public class Day11 {
 	static final char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-	String input = "vzbxkghb";
+	static String input = "vzbxkghb";
 
 	public static void run() {
-		String shifted = shift("zebra");
-		System.out.println(shifted);
+		
 	}
 
 	static String shift(String s) {
 		char[] parts = s.toCharArray();
 		for (int i = 0; i < parts.length; i++) {
 			char curr = parts[parts.length - 1 - i]; // extract for easier use
-			if (curr == 'z') { // wrap around
-				curr = 'a';
-			} else { // get next letter a->b
-				int index = findAlphabetIndex(curr);
-				curr = alphabet[index + 1];
-			}
+			char newChar = getNextChar(curr);
 			// insert in parts again
-			parts[parts.length - 1 - i] = curr;
+			parts[parts.length - 1 - i] = newChar;
 		}
 		return String.valueOf(parts);
 	}
@@ -32,5 +26,51 @@ public class Day11 {
 				return i;
 		System.out.println("index not found: '" + search + "'");
 		return -1;
+	}
+
+	static char getNextChar(char c) {
+		if (c == 'z') { // wrap around
+			c = 'a';
+		} else { // get next letter a->b
+			int index = findAlphabetIndex(c);
+			c = alphabet[index + 1];
+		}
+		return c;
+	}
+
+	static boolean validate(String s) {
+		boolean valid1 = false, valid2 = true, valid3 = false;
+		char[] parts = s.toCharArray();
+		// validate 1: has to contain abc or fgh etc.
+		System.out.println(String.valueOf(parts));
+		for (int i = 0; i < parts.length - 2; i++) {
+			char curr = parts[i];
+			char curr1 = parts[i + 1]; // curr + 1
+			char curr2 = parts[i + 2]; // curr + 2
+			if (getNextChar(curr) == curr1 && getNextChar(curr1) == curr2) {
+				valid1 = true;
+				break;
+			}
+		}
+
+		// validate 2: pw may not contain i, o or l
+		for (char c : parts)
+			if (c == 'i' || c == 'o' || c == 'l')
+				valid2 = false;
+
+		// validate 3:
+		char validChar1 = 0;
+		for (int i = 0; i < parts.length - 1; i++) {
+			if (parts[i] == parts[i + 1])
+				validChar1 = parts[i];
+
+		}
+		for (int j = 0; j < parts.length - 1; j++) { // same thing again, but this time exlude validChar
+			if (parts[j] == parts[j + 1] && parts[j] != validChar1) // found a second different pair
+				valid3 = true;
+		}
+
+		System.out.println("Validation: " + valid1 + ", " + valid2 + " and " + valid3);
+		return valid1 && valid2 && valid3; // all three have to be true;
 	}
 }
